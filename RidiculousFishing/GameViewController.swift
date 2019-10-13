c901017bae3f1be3def74db62e802edeaded3a87
INDRAVADAN SHRIMALI <indravadanshrimali@hotmail.com>
2019-10-13 19:10:51 -0400

    fishes added

--- RidiculousFishing/GameViewController.swift
+++ RidiculousFishing/GameViewController.swift
@@ -15,25 +15,17 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-            
-            // SHOW HITBOXES
-            view.showsPhysics = true
-            
-        }
+        let scene = GameScene(size:self.view.bounds.size)
+        let skView = self.view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .aspectFill
+        
+        // property to show hitboxes
+        skView.showsPhysics = true
+        
+        skView.presentScene(scene) 
     }
 
     override var shouldAutorotate: Bool {
